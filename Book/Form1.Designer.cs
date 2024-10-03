1eeb62a89c7a332d3b772c7d6933398944391b4f
ParanovaFactory <berkay_krdmn@hotmail.com>
2024-10-03 15:44:42 +0300

    Add project files.

---
+++ Book/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Book
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.lblbook = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtPublisher = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.mskPage = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 193);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(661, 249);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Book List";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 448);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(661, 260);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Deleted Book List";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(205, 57);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(98, 39);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(655, 224);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 22);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(655, 235);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // lblbook
+            // 
+            this.lblbook.AutoSize = true;
+            this.lblbook.Location = new System.Drawing.Point(148, 170);
+            this.lblbook.Name = "lblbook";
+            this.lblbook.Size = new System.Drawing.Size(18, 20);
+            this.lblbook.TabIndex = 4;
+            this.lblbook.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Number of books";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(205, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(98, 39);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(99, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 26);
+            this.txtName.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Author";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Page";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Publisher";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Type";
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(99, 44);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(100, 26);
+            this.txtAuthor.TabIndex = 13;
+            // 
+            // txtPublisher
+            // 
+            this.txtPublisher.Location = new System.Drawing.Point(99, 109);
+            this.txtPublisher.Name = "txtPublisher";
+            this.txtPublisher.Size = new System.Drawing.Size(100, 26);
+            this.txtPublisher.TabIndex = 15;
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(99, 141);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(100, 26);
+            this.txtType.TabIndex = 16;
+            // 
+            // mskPage
+            // 
+            this.mskPage.Location = new System.Drawing.Point(99, 77);
+            this.mskPage.Mask = "0000";
+            this.mskPage.Name = "mskPage";
+            this.mskPage.Size = new System.Drawing.Size(100, 26);
+            this.mskPage.TabIndex = 14;
+            this.mskPage.ValidatingType = typeof(int);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 716);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.txtPublisher);
+            this.Controls.Add(this.mskPage);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblbook);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblbook;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtPublisher;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.MaskedTextBox mskPage;
+    }
+}
+
---
+++ Book/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Book
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection sql = new SqlConnection(@"Data Source=predator;Initial Catalog=Db_Book;Integrated Security=True;TrustServerCertificate=True");
+
+        void listBook()
+        {
+            sql.Open();
+            SqlCommand cmd = new SqlCommand("select * from TblBook", sql);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            sql.Close();
+        }
+
+        void listDeletedBook()
+        {
+            sql.Open();
+            SqlCommand cmd = new SqlCommand("select * from Tbl_DeletedBook", sql);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView2.DataSource = dt;
+            sql.Close();
+        }
+
+        void listBookCount()
+        {
+            sql.Open();
+            SqlCommand cmd = new SqlCommand("select * from TblNumOfBook", sql);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                lblbook.Text = reader[0].ToString();
+            }
+            sql.Close();
+        }
+
+        void clear()
+        {
+            txtName.Clear();
+            txtAuthor.Clear();
+            mskPage.Clear();
+            txtPublisher.Clear();
+            txtType.Clear();
+        }
+
+        string id, name, author, page, publisher, type;
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sql.Open();
+                SqlCommand cmd = new SqlCommand("insert into TblBook (bookName,bookAuthor,bookPage,publishingHouse,bookType) values(@p1,@p2,@p3,@p4,@p5)", sql);
+                cmd.Parameters.AddWithValue("@p1", txtName.Text);
+                cmd.Parameters.AddWithValue("@p2", txtAuthor.Text);
+                cmd.Parameters.AddWithValue("@p3", mskPage.Text);
+                cmd.Parameters.AddWithValue("@p4", txtPublisher.Text);
+                cmd.Parameters.AddWithValue("@p5", txtType.Text);
+                cmd.ExecuteNonQuery();
+                sql.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Chech the values");
+            }
+
+
+            listBook();
+            listDeletedBook();
+            listBookCount();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sql.Open();
+                SqlCommand command = new SqlCommand("delete from TblBook where bookId = @p1", sql);
+                command.Parameters.AddWithValue("@p1", int.Parse(id));
+                command.ExecuteNonQuery();
+                sql.Close();
+
+                sql.Open();
+                SqlCommand command1 = new SqlCommand("insert into Tbl_DeletedBook (bookDeleteName,bookDeleteAuthor,bookDeletePage,bookDeletePublishingHouse,bookDeleteType) values(@p2,@p3,@p4,@p5,@p6)", sql);
+                command1.Parameters.AddWithValue("@p2", name);
+                command1.Parameters.AddWithValue("@p3", author);
+                command1.Parameters.AddWithValue("@p4", page);
+                command1.Parameters.AddWithValue("@p5", publisher);
+                command1.Parameters.AddWithValue("@p6", type);
+                command1.ExecuteNonQuery();
+                sql.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Cilich a value form book list");
+            }
+            listBook();
+            listDeletedBook();
+            listBookCount();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBook();
+            listDeletedBook();
+            listBookCount();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            id = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            name = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            author = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            page = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            publisher = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            type = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+        }
+    }
+}
